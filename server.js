216a9f1c697ff658fcb2bdc972c1262d01bba910
Krishna Shahi <krishnashahi3888@gmail.com>
2018-03-28 03:22:49 +0530

    adding background image on web page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
 var port=3001;
-app.use(express.static(_dirname_+'/'));
+app.use(express.static('workshopWD'));
 app.listen(port);
 console.log('Server started at'+port);
